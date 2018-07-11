edb0ee868a658755d45500184f711dc0174c2d70
Juan Carlos Campos <rapidclimate@gmail.com>
2018-07-11 11:59:46 -0500

    Android Mensajitos

--- app/src/main/java/org/unitec/androidmensajitos/MainActivity.kt
+++ app/src/main/java/org/unitec/androidmensajitos/MainActivity.kt
@@ -1,12 +1,21 @@
 package org.unitec.androidmensajitos
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //manejamos el evento de boton igual de manera funcional como en spring
+
+        var boton=findViewById<Button>(R.id.ingresar).setOnClickListener{
+      var   intent= Intent(applicationContext, MenuActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
